9caa571ef3855c46fa4a71ba7fb690910f250893
Gutnikova Olga <olichikf1@mail.ru>
2023-02-13 12:13:28 +0300

    Решение задачи

--- task_final/Program.cs
+++ task_final/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getUserNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+string[] enterArray(int length)
+{
+    string[] array = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-й элемент массива: ");
+        array[i] = (Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] SecondArray(string[] array1)
+{
+    string[] array2 = new string[array1.Length];
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+    return array2;
+}
+int N = getUserNumber("Введите количество элементов массивы: ");
+string[] Array1 = enterArray(N);
+Console.Write("Исходный массив: ");
+PrintArray(Array1);
+string[] SecondAr = SecondArray(Array1);
+Console.Write("Итоговый массив: ");
+PrintArray(SecondAr);
